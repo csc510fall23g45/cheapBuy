424e971acc9c846726cb84bcf0f9e6577779af31
NITYA NAGA SAI ATLURI <nityanagasai@gmail.com>
2023-11-27 19:44:39 -0500

    Added Licence

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2023 Group45
+This code is licensed under MIT license (see LICENSE.MD for details)
+
+@author: cheapBuy
+"""
+
 from setuptools import setup
 
 setup(name='cheapBuy',
